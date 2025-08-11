07b1e143875845ac95a5ce88db7db83ce1b1e786
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-08-11 17:46:09 +0200

    deleted unnecesary assets

--- react-discord-hero/src/App.jsx
+++ react-discord-hero/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 // import './App.css'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 function App() {
   return (
-    <>
+      // <div className="App">
+      //   <Hero />
+      //   <Navbar />
+      // </div>
+      <>
       <Hero />
       <Navbar />
-    </>
+      </>
+
   )
 }
 
