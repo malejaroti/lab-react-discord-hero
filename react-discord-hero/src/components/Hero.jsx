48024ff71a6fe4a9a855ebd02f01e5ef89cf785e
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-08-11 18:07:11 +0200

    added media queries to have a design adjusted to laptop and monitor screens

--- react-discord-hero/src/components/Hero.jsx
+++ react-discord-hero/src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero(){
             <div className="hero__content">
                 <h1>IMAGINE A PLACE...</h1>
                 <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend tiem together. A place that makes it easy to talk ever day and hang out more often.</p>
-                <div className="hero__actions">
+                <div className="hero__buttons">
                     <button className="btn btn-mac">Download for Mac</button>
                     <button className="btn btn-browser">Open Discord in your browser</button>
                 </div>
